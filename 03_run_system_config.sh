640fcffcc038ba73e63dce93a1b7e88972484a0c
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2022-03-01 15:58:20 -0300

    Improve 03_run_system_config

--- 03_run_system_config.sh
+++ 03_run_system_config.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+cp -ivf ~/.bashrc ~/.bashrc.bkp
+
 # Enable bash colors
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc && . ~/.bashrc
 
+apt install -y vim \
+    software-properties-common \
+    python-software-properties \
+    meld \
+    terminator \
+    tree \
+    screenfetch \
+    htop \
+    gource \
+    curl \
+    xsel \
+    lynx
