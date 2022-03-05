a9ff5f6c058f0c231d5273c8e36aa904e4dc1086
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2022-03-05 16:53:03 -0300

    Add .gitconfig

--- 01_run_dotfiles_config.sh
+++ 01_run_dotfiles_config.sh
@@ -3,11 +3,13 @@
 # Backup current dot files before applying new configs
 mv -vf ~/.bash_aliases ~/.bash_aliases_bkp
 mv -vf ~/.vimrc ~/.vimrc_bkp
+mv -vf ~/.gitconfig ~/.gitconfig_bkp
 cp -ivf ~/.bashrc ~/.bashrc_bkp
 
 # Configure dot files
-ln -s ~/git/my-dot-files/.bash_aliases .bash_aliases
-ln -s ~/git/my-dot-files/.vimrc .vimrc
+ln -s ~/git/my-dot-files/.bash_aliases ~/.bash_aliases
+ln -s ~/git/my-dot-files/.vimrc ~/.vimrc
+ln -s ~/git/my-dot-files/.gitconfig ~/.gitconfig
 
 # Enable bash colors
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc && . ~/.bashrc
