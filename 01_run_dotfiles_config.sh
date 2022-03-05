264c2b260bfa843f4b5af93d88bb4a156f0fbfc2
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2022-03-05 17:09:41 -0300

    Refactoring

--- 01_run_dotfiles_config.sh
+++ 01_run_dotfiles_config.sh
@@ -8,10 +8,10 @@ mv -vf ~/.config/terminator/config ~/.config/terminator/config_bkp
 cp -ivf ~/.bashrc ~/.bashrc_bkp
 
 # Configure dot files
-ln -s ~/git/my-dot-files/.bash_aliases ~/.bash_aliases
-ln -s ~/git/my-dot-files/.vimrc ~/.vimrc
-ln -s ~/git/my-dot-files/.gitconfig ~/.gitconfig
-ln -s ~/git/my-dot-files/terminator/config ~/.config/terminator/config
+ln -s ~/git/my-dot-files/config/.bash_aliases ~/.bash_aliases
+ln -s ~/git/my-dot-files/config/.vimrc ~/.vimrc
+ln -s ~/git/my-dot-files/config/.gitconfig ~/.gitconfig
+ln -s ~/git/my-dot-files/config/terminator/config ~/.config/terminator/config
 
 # Enable bash colors
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc && . ~/.bashrc
--- 02_run_git_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$(git config --global user.name "Diogo Alexsander Cavilha")
-$(git config --global user.email "diogocavilha@gmail.com")
-$(git config --global color.ui auto)
-$(git config --global core.editor "vim")
-$(git config --global diff.tool meld)
-$(git config --global push.default simple)
-$(git config --global mergetool.keepBackup false)
