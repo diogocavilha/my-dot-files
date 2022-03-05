a048e7c8e45a346ce6d0e1a9d06d24249a92ba84
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2022-03-05 17:00:55 -0300

    Add terminator config

--- 01_run_dotfiles_config.sh
+++ 01_run_dotfiles_config.sh
@@ -4,12 +4,14 @@
 mv -vf ~/.bash_aliases ~/.bash_aliases_bkp
 mv -vf ~/.vimrc ~/.vimrc_bkp
 mv -vf ~/.gitconfig ~/.gitconfig_bkp
+mv -vf ~/.config/terminator/config ~/.config/terminator/config_bkp
 cp -ivf ~/.bashrc ~/.bashrc_bkp
 
 # Configure dot files
 ln -s ~/git/my-dot-files/.bash_aliases ~/.bash_aliases
 ln -s ~/git/my-dot-files/.vimrc ~/.vimrc
 ln -s ~/git/my-dot-files/.gitconfig ~/.gitconfig
+ln -s ~/git/my-dot-files/terminator/config ~/.config/terminator/config
 
 # Enable bash colors
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc && . ~/.bashrc
