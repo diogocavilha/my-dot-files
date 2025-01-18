7cc4620f1055492e9f68b5397bb1a0f525f4e72d
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2025-01-18 14:14:23 -0300

    chore: add mint install packages

---
+++ mint_install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# brave browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
+apt install -y build-essential \
+    git \
+    vim \
+    terminator \
+    htop \
+    rar \
+    unrar \
+    redshift \
+    redshift-gtk
+
